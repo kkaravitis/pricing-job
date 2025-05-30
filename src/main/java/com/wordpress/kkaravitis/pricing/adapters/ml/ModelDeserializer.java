efd1252125a3a9428ab564bc30e1612aeae428f8
Konstantinos Karavitis <kkaravitis@gmail.com>
2025-05-30 16:02:43 +0300

    Added Licence header in files.

--- src/main/java/com/wordpress/kkaravitis/pricing/adapters/ml/ModelDeserializer.java
+++ src/main/java/com/wordpress/kkaravitis/pricing/adapters/ml/ModelDeserializer.java
@@ -1,16 +1,34 @@
+/*
+ * Copyright 2025 Konstantinos Karavitis
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.wordpress.kkaravitis.pricing.adapters.ml;
 
-import com.wordpress.kkaravitis.pricing.domain.*;
+import com.wordpress.kkaravitis.pricing.domain.Money;
+import com.wordpress.kkaravitis.pricing.domain.PricingRuntimeException;
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.math.BigDecimal;
-import java.nio.file.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
 import java.util.Map;
-import java.util.Optional;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipInputStream;
-
-import org.tensorflow.*;
+import org.tensorflow.SavedModelBundle;
+import org.tensorflow.SessionFunction;
+import org.tensorflow.Tensor;
 import org.tensorflow.ndarray.NdArrays;
 import org.tensorflow.ndarray.Shape;
 import org.tensorflow.types.TFloat32;
